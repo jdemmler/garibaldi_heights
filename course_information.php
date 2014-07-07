a41325535b2d180f38a9a054bbad5fff8e6e1b93
jdemmler <joannedemmler@gmail.com>
2014-07-07 16:13:34 -0700

    social icons and course info page

--- course_information.php
+++ course_information.php
@@ -23,53 +23,99 @@ include('./templates/header.php');
 
 	<section class="information_container">
 		<div class="inner_information">
+			<div class="ci_container">
+				<div class="picture">
+					<article class="restaurant general_image_settings_ci">
 
-			<article class="restaurant general_image_settings_ci">
+						<img class="restaurant_trans01" src="images/heights_restaurant.jpg" alt="Picture of Heights Restaurant">
 
-				<img class="restaurant_trans01" src="images/heights_restaurant.jpg" alt="Picture of Heights Restaurant">
+						<figcaption class="course_info_transbox restaurant_trans02">
 
-				<figcaption class="course_info_transbox restaurant_trans02">
+							<h1 class="restaurant_trans03">Heights Restaurant</h1>
+							<h2 class="restaurant_trans03">Opening Hours</h2>
+							<p class="restaurant_trans03">8:00am to 11:00pm</p>	
 
-					<h1 class="restaurant_trans03">Heights Restaurant</h1>
-					<h2 class="restaurant_trans03">Opening Hours</h2>
-					<p class="restaurant_trans03">8:00am to 11:00pm</p>	
+						</figcaption><!-- end course_info_transbox -->
 
-				</figcaption><!-- end course_info_transbox -->
+					</article><!-- end restaurant -->
 
-				<p>Lorem Ipsum test transition Seymour street align Sydney social media tic-tac-toe mobile! Internet Explorer design footer while Clarice and coffee break "H: drive" so Denielle. Javascript assignment "Cascading Style Sheets" Photoshop zip float-right proposal? Markup Dreamweaver rotate wiki projector tags text file, to the downtown campus the direct select Samantha mobile. Nav Mark eraser Peter cross-browser Matthew Alexander. Navigation SASS pencil while coffee break while Aimee reboot stroke Sisi. File Carla assess anchor point, from Sara box model link to Erica Christine mockup "Delete all files on the FTP server"! Student lounge Firefox Mac be feed dropbox mockup BCIT display-block, present wordpress click icons vendor-prefix hover over</P>
+				</div><!-- end picture -->
+
+				<article class="information_speil">
+
+					<p>Lorem Ipsum test transition Seymour street align Sydney social media tic-tac-toe mobile! Internet Explorer design footer while Clarice and coffee break "H: drive" so Denielle. Javascript assignment "Cascading Style Sheets" Photoshop zip float-right proposal? Markup Dreamweaver rotate wiki projector tags text file, to the downtown campus the direct select Samantha mobile. Nav Mark eraser Peter cross-browser Matthew Alexander. Navigation SASS pencil while coffee break while Aimee reboot stroke Sisi. File Carla assess anchor point, from Sara box model link to Erica Christine mockup "Delete all files on the FTP server"! Student lounge Firefox Mac be feed dropbox mockup BCIT display-block, present wordpress click icons vendor-prefix hover over.</p>
+
+					<p>Responsive blog Matthew rotate jpg design Sara direct select. Code Samantha stroke in jQuery HTML zip Adobe Clarice! Test font-face hover over the vendor-prefix coffee break dropbox.</p>
+
+				</article><!-- end information speil -->
+
+				<div class="clear">
+				</div><!-- end of clear -->
+</div><!-- end ci_container -->
+
+				<div class="ci_container">
+					<div class="picture">
+
+						<article class="pro_shop general_image_settings_ci">
+
+							<img class="pro_shop_trans01" src="images/pro_shop.jpg" alt="Picture of Pro Shop">
+
+							<figcaption class="course_info_transbox pro_shop_trans02">
+
+								<h1 class="pro_shop_trans03">Pro Shop</h1>
+								<h2 class="pro_shop_trans03">Opening Hours</h2>
+								<p class="pro_shop_trans03">6:00am to 9:00pm</p>
+
+							</figcaption><!-- course_info_transbox -->
+
+						</article><!-- end pro_shop -->
+
+					</div><!-- end picture -->
+
+					<article class="information_speil">
+
+						<p>Lorem Ipsum test transition Seymour street align Sydney social media tic-tac-toe mobile! Internet Explorer design footer while Clarice and coffee break "H: drive" so Denielle. Javascript assignment "Cascading Style Sheets" Photoshop zip float-right proposal? Markup Dreamweaver rotate wiki projector tags text file, to the downtown campus the direct select Samantha mobile. Nav Mark eraser Peter cross-browser Matthew Alexander. Navigation SASS pencil while coffee break while Aimee reboot stroke Sisi. File Carla assess anchor point, from Sara box model link to Erica Christine mockup "Delete all files on the FTP server"! Student lounge Firefox Mac be feed dropbox mockup BCIT display-block, present wordpress click icons vendor-prefix hover over.</p>
+
+						<p>Responsive blog Matthew rotate jpg design Sara direct select. Code Samantha stroke in jQuery HTML zip Adobe Clarice! Test font-face hover over the vendor-prefix coffee break dropbox.</p>
+
+					</article><!-- end information speil -->
+
 					<div class="clear">
 					</div><!-- end of clear -->
 
-				</article><!-- end restaurant -->
+				</div><!-- end ci_container -->
 
-				<article class="pro_shop general_image_settings_ci">
+				<div class="ci_container">
+					<div class="picture">
 
-					<img class="pro_shop_trans01" src="images/pro_shop.jpg" alt="Picture of Pro Shop">
+						<article class="green_fees general_image_settings_ci">
 
-					<figcaption class="course_info_transbox pro_shop_trans02">
+							<img class="green_fees_trans01" src="images/green_fees.jpg" alt="Green Fees">
 
-						<h1 class="pro_shop_trans03">Pro Shop</h1>
-						<h2 class="pro_shop_trans03">Opening Hours</h2>
-						<p class="pro_shop_trans03">6:00am to 9:00pm</p>
+							<figcaption class="course_info_transbox green_fees_trans02">
 
-					</figcaption><!-- course_info_transbox -->
+								<h1 class="green_fees_trans03">Green Fees</h1>
+								<p class="green_fees_trans03">18 Holes $30</p>
+								<p class="green_fees_trans03">9 Holes $20</p>
 
-				</article><!-- end pro_shop -->
+							</figcaption><!-- end course_info_transbox -->
 
-				<article class="green_fees general_image_settings_ciS">
+						</article><!-- end green_fees -->
+					</div><!-- end picture -->
 
-					<img class="green_fees_trans01" src="images/green_fees.jpg" alt="Green Fees">
+					<article class="information_speil">
 
-					<figcaption class="course_info_transbox green_fees_trans02">
+						<p>Lorem Ipsum test transition Seymour street align Sydney social media tic-tac-toe mobile! Internet Explorer design footer while Clarice and coffee break "H: drive" so Denielle. Javascript assignment "Cascading Style Sheets" Photoshop zip float-right proposal? Markup Dreamweaver rotate wiki projector tags text file, to the downtown campus the direct select Samantha mobile. Nav Mark eraser Peter cross-browser Matthew Alexander. Navigation SASS pencil while coffee break while Aimee reboot stroke Sisi. File Carla assess anchor point, from Sara box model link to Erica Christine mockup "Delete all files on the FTP server"! Student lounge Firefox Mac be feed dropbox mockup BCIT display-block, present wordpress click icons vendor-prefix hover over.</p>
 
-						<h1 class="green_fees_trans03">Green Fees</h1>
-						<p class="green_fees_trans03">18 Holes $30</p>
-						<p class="green_fees_trans03">9 Holes $20</p>
+						<p>Responsive blog Matthew rotate jpg design Sara direct select. Code Samantha stroke in jQuery HTML zip Adobe Clarice! Test font-face hover over the vendor-prefix coffee break dropbox.</p>
 
-					</figcaption><!-- end course_info_transbox -->
 
-				</article><!-- end green_fees -->
+					</article><!-- end information speil -->
 
+					<div class="clear">
+					</div><!-- end of clear -->
+
+				</div><!-- end ci_container -->
 
 				<div class="clear">
 				</div><!-- end of clear -->
